a02c6b4ac339e8a54379c9b02e15a9cee0d88099
KaungMyat <aungakm667@gmail.com>
2023-05-07 13:25:50 +0630

    fix deploy error

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { useEffect, useState, useRef } from "react";
+
+import { useEffect, useState} from "react";
 import { IoIosArrowDropleftCircle } from "react-icons/io";
 import Youtube from "react-youtube";
 interface props {
