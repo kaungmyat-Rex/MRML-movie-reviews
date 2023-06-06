8ae7f6c51fe607c7fdefbc23c82f6af68b9ccc97
KaungMyat <aungakm667@gmail.com>
2023-06-06 23:47:42 +0630

    add manifest.json files

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name][extname]",
+      },
+    },
+  },
 })
